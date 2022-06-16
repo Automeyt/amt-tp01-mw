e018c848941598de7f0b41a0153d8ba5d622ca1e
Mike Navales <mike@mikenavales.xyz>
2022-06-16 13:19:42 +0800

    Add footer content + social media icons

--- js/main.js
+++ js/main.js
@@ -23,10 +23,65 @@ class AmtHeader extends HTMLElement {
 class AmtFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <footer class="">
-                <p href="#" class="">
-                    Copyright © 2022 Automeyt.
-                </p>
+            <footer class="bg-black text-white grid grid-flow-col auto-cols-max">
+                <div class="grid grid-flow-col auto-cols-max">
+                    <!-- Upwork Icon -->
+                    <a href="https://www.upwork.com/freelancers/~0105b4581377de52ba" rel="noopener noreferrer" target="_blank">
+                        <button class="
+                                bg-white
+                                text-black
+                                shadow-lg
+                                font-normal
+                                h-10
+                                w-10
+                                rounded-full
+                                outline-none
+                                mr-2
+                                p-3
+                            " type="button" aria-label="Explore Upwork Profile">
+                            <img src="images/upwork-icon.svg" alt="" width="16px" height="16px">
+                            </button>
+                        </a>
+                    <!-- Facebook Icon -->
+                    <a href="https://www.facebook.com/automeyt" rel="noopener noreferrer" target="_blank">
+                        <button class="
+                            bg-white
+                            text-blue-600
+                            shadow-lg
+                            font-normal
+                            h-10
+                            w-10
+                            rounded-full
+                            outline-none
+                            mr-2
+                            p-3
+                        " type="button" aria-label="Explore Facebook Page">
+                        <img src="images/facebook-icon.svg" alt="" width="16px" height="16px">
+                        </button>
+                    </a>
+                    <!-- Linkedin Icon -->
+                    <a href="https://www.linkedin.com/in/kyooreas/" rel="noopener noreferrer" target="_blank">
+                        <button class="
+                            bg-white
+                            text-black
+                            shadow-lg
+                            font-normal
+                            h-10
+                            w-10
+                            rounded-full
+                            outline-none
+                            mr-2
+                            p-3
+                        " type="button" aria-label="Explore Linkedin Profile">
+                        <img src="images/linkedin-icon.svg" alt="" width="16px" height="16px">
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <p href="#" class="">
+                        Copyright © 2022 Automeyt.
+                    </p>
+                </div>
             </footer>
         `
     }
