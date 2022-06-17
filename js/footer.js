0c3c596d941d1a910d3ef3a04a351ac480081677
Mike Navales <mike@mikenavales.xyz>
2022-06-17 15:08:38 +0800

    Center socia media icons and copyright text

--- js/footer.js
+++ js/footer.js
@@ -11,10 +11,45 @@ class AmtFooter extends HTMLElement {
                         </div>
                     </section>
                     <hr class="my-6 border-gray-400">
-                    <section class="grid grid-flow-col auto-cols-max py-4 bg-blue">
-                        <!-- Upwork Icon -->
-                        <a href="https://www.upwork.com/freelancers/~0105b4581377de52ba" rel="noopener noreferrer" target="_blank" class="">
-                            <button class="
+                    <section class="">
+                        <div class="grid grid-flow-col auto-cols-max place-content-center py-1">
+                            <!-- Upwork Icon -->
+                            <a href="https://www.upwork.com/freelancers/~0105b4581377de52ba" rel="noopener noreferrer" target="_blank" class="">
+                                <button class="
+                                        bg-white
+                                        text-black
+                                        shadow-lg
+                                        font-normal
+                                        h-10
+                                        w-10
+                                        rounded-full
+                                        outline-none
+                                        mr-2
+                                        p-3
+                                    " type="button" aria-label="Explore Upwork Profile">
+                                    <img src="images/upwork-icon.svg" alt="" width="16px" height="16px">
+                                    </button>
+                                </a>
+                            <!-- Facebook Icon -->
+                            <a href="https://www.facebook.com/automeyt" rel="noopener noreferrer" target="_blank">
+                                <button class="
+                                    bg-white
+                                    text-blue-600
+                                    shadow-lg
+                                    font-normal
+                                    h-10
+                                    w-10
+                                    rounded-full
+                                    outline-none
+                                    mr-2
+                                    p-3
+                                " type="button" aria-label="Explore Facebook Page">
+                                <img src="images/facebook-icon.svg" alt="" width="16px" height="16px">
+                                </button>
+                            </a>
+                            <!-- Linkedin Icon -->
+                            <a href="https://www.linkedin.com/in/kyooreas/" rel="noopener noreferrer" target="_blank">
+                                <button class="
                                     bg-white
                                     text-black
                                     shadow-lg
@@ -25,50 +60,18 @@ class AmtFooter extends HTMLElement {
                                     outline-none
                                     mr-2
                                     p-3
-                                " type="button" aria-label="Explore Upwork Profile">
-                                <img src="images/upwork-icon.svg" alt="" width="16px" height="16px">
+                                " type="button" aria-label="Explore Linkedin Profile">
+                                <img src="images/linkedin-icon.svg" alt="" width="16px" height="16px">
                                 </button>
                             </a>
-                        <!-- Facebook Icon -->
-                        <a href="https://www.facebook.com/automeyt" rel="noopener noreferrer" target="_blank">
-                            <button class="
-                                bg-white
-                                text-blue-600
-                                shadow-lg
-                                font-normal
-                                h-10
-                                w-10
-                                rounded-full
-                                outline-none
-                                mr-2
-                                p-3
-                            " type="button" aria-label="Explore Facebook Page">
-                            <img src="images/facebook-icon.svg" alt="" width="16px" height="16px">
-                            </button>
-                        </a>
-                        <!-- Linkedin Icon -->
-                        <a href="https://www.linkedin.com/in/kyooreas/" rel="noopener noreferrer" target="_blank">
-                            <button class="
-                                bg-white
-                                text-black
-                                shadow-lg
-                                font-normal
-                                h-10
-                                w-10
-                                rounded-full
-                                outline-none
-                                mr-2
-                                p-3
-                            " type="button" aria-label="Explore Linkedin Profile">
-                            <img src="images/linkedin-icon.svg" alt="" width="16px" height="16px">
-                            </button>
-                        </a>
+                        </div>
+                        <div class="grid grid-flow-col auto-cols-max place-content-center py-1">
+                            <a href="#" class="grid content-center text-sm text-gray-600 font-semibold">
+                                Copyright © 2022 Automeyt.
+                            </a>
+                        </div>
                     </section>
-                    <div>
-                        <a href="#" class="grid content-center">
-                            Copyright © 2022 Automeyt.
-                        </a>
-                    </div>
+                    
                 </div>
             </footer>
         `
