e1fb3186abe7378a90caaf5a23972614457323de
dbigCodes <jesrelmalaqui@gmail.com>
2022-06-17 16:35:30 +0800

    added the section with 3 cards, focus, integrate, and tailor automated solutions

--- js/home.js
+++ js/home.js
@@ -15,6 +15,41 @@ class AmtHome extends HTMLElement {
                 </div>
             </article>
         </section>
+
+        <section class="pb-20 bg-gray-300 -mt-24">
+        <article class="container mx-auto px-4">
+            <div class="grid md:grid-cols-3 text-center">
+                <!--col-1-->
+                <div class="pt-28 w-full md:4/12 px-4">
+                    <div class="grid place-items-center relative min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg p-4 ">
+                        <div class=" p-3 w-12 h-12 mb-5 shadow-lg rounded-full bg-orange bg-opacity-70 text-center">
+                            <img src="images/focus-icon.svg" alt="Focus on things that matter" width="24px" height="24px">
+                        </div>
+                        <h2 class="text-xl font-semibold">Focus on things that matter</h2>
+                        <p class="mt-2 mb-4 text-gray-600"> We will set it up and Zapier will handle the work for you automatically, so you can focus more on the important tasks within the business.</p>
+                    </div>
+                </div>
+                 <!--col-2-->
+                 <div class="pt-20 w-full md:4/12 px-4">
+                    <div class="grid place-items-center relative min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg p-4 ">
+                        <div class=" p-3 w-12 h-12 mb-5 shadow-lg rounded-full bg-orange bg-opacity-70 text-center">
+                            <img src="images/transfer-icon.svg" alt="Focus on things that matter" width="24px" height="24px">
+                        </div>
+                        <h2 class="text-xl font-semibold">Integrate with 3000+ Apps</h2>
+                        <p class="mt-2 mb-4 text-gray-600"> Stick with the tools that work for you. Zapier connects more web apps than anyone, and they add new options every week.</p>
+                    </div>
+                </div>
+                 <!--col-3-->
+                 <div class="pt-10 w-full md:4/12 px-4">
+                    <div class="grid place-items-center relative min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg p-4 ">
+                        <div class=" p-3 w-12 h-12 mb-5 shadow-lg rounded-full bg-orange bg-opacity-70 text-center">
+                            <img src="images/transfer-icon.svg" alt="Focus on things that matter" width="24px" height="24px">
+                        </div>
+                        <h2 class="text-xl font-semibold">Tailor automated solutions</h2>
+                        <p class="mt-2 mb-4 text-gray-600">Based on your team's needs, we can set up the automation that will fit to your processes and workflows.</p>
+                    </div>
+                </div>
+            </div>
         `
     }
 }
