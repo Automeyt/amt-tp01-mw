bbdeff97a2ece923f0e93a360287fcde6a72f083
Mike Navales <mike@automeyt.com>
2022-06-24 18:28:01 +0800

    Add responsiveness by hiding hamburger icon after lg:

--- js/header.js
+++ js/header.js
@@ -2,22 +2,16 @@ class AmtHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <header class="bg-black sticky top-0 z-50">
-                <div class="container mx-auto grid grid-cols-2 p-6">
+                <div class="container mx-auto relative grid grid-flow-col auto-cols-max place-content-between p-6">
                     <div class="grid place-content-start content-center"> 
                         <a href="#" class="">
                             <img src="images/automeyt-logo-black-bg.webp" class="h-10" alt="Automeyt Logo">
                         </a>
                     </div>
-                    <nav class="relative">
-                        <div class="grid place-content-end content-center p-4 space-y-2 rounded shadow"
-                            onclick="toggleNav()">
-                            <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                            <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                            <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
-                        </div>
-                        <ul class="hidden absolute right-0 w-full rounded-b-lg grid lg:grid-flow-col lg:auto-cols-max 
-                            lg:gap-6 py-2 text-center text-white bg-orange"
-                            id="nav_list">
+                    <nav class="hidden lg:grid" id="nav_list">
+                        <ul class="absolute lg:relative right-0 w-full rounded-b-lg lg:grid 
+                            lg:grid-flow-col lg:auto-cols-max lg:gap-6 py-2 text-center 
+                            text-white lg: text-xl bg-orange lg:bg-transparent">
                             <li >
                                 <a href="#">Home</a> 
                             </li>
@@ -38,6 +32,12 @@ class AmtHeader extends HTMLElement {
                             </li>
                         </ul>
                     </nav>
+                    <div class="lg:hidden grid place-content-end p-4 space-y-2 rounded shadow"
+                            onclick="toggleNav()">
+                        <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+                        <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+                        <span class="block w-8 h-0.5 bg-gray-100 animate-pulse"></span>
+                    </div>
                 </div>
             </header>
         `
